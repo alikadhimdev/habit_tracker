160958d8e92e8778f14cbb5dad9fa33b36c0a404
ali kadhim <ali20140022@gmail.com>
2025-08-17 21:08:48 +0300

    complete calender screen design

--- lib/screens/calender_screen.dart
+++ lib/screens/calender_screen.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class CalenderScreen extends StatelessWidget {
-  const CalenderScreen({super.key});
+  CalenderScreen({super.key});
+
+  final List<String> _filtersName = ["جميع الفلاتر", "التمارين", "القراءة"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("صفحة القويم"));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("الفلاتر"),
+            SizedBox(height: 10),
+            Container(
+              height: 30,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 3,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+
+                    child: Text(
+                      _filtersName[index],
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TableCalendar(
+              focusedDay: DateTime.now(),
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              rowHeight: 35,
+            ),
+
+            SizedBox(height: 10),
+            Text("التقدم", style: Theme.of(context).textTheme.headlineSmall),
+            SizedBox(height: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("التقدم الاسبوعي"),
+                Text("60%", style: Theme.of(context).textTheme.displaySmall),
+                Container(
+                  child: Row(
+                    children: [
+                      Text(
+                        "اخر 7 ايام",
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      Text(
+                        "+ 10 %",
+                        style: TextStyle(color: Colors.green[700]),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+
+            Container(
+              height: 200,
+              child: LineChart(
+                LineChartData(
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: [
+                        FlSpot(0, 3),
+                        FlSpot(1, 1),
+                        FlSpot(2, 4),
+                        FlSpot(3, 2),
+                        FlSpot(4, 5),
+                      ],
+                      isCurved: true,
+                      dotData: FlDotData(show: true),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
