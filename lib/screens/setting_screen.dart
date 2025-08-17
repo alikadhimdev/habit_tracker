ef6cf7c574f2c1f2e81aedb8c6394a773d0c42a6
ali kadhim <ali20140022@gmail.com>
2025-08-17 21:52:23 +0300

    complete settings screen design

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,10 +1,121 @@
 import 'package:flutter/material.dart';
 
-class SettingScreen extends StatelessWidget {
+class SettingScreen extends StatefulWidget {
   const SettingScreen({super.key});
 
   @override
+  State<SettingScreen> createState() => _SettingScreenState();
+}
+
+class _SettingScreenState extends State<SettingScreen> {
+  String? _themeChoose = "system";
+  bool _notifications = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text(" صفحة الاعدادات"));
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text("الحساب", style: Theme.of(context).textTheme.headlineSmall),
+          SizedBox(height: 10),
+          ListTile(
+            leading: CircleAvatar(child: Image.asset("images/image1.jpeg")),
+            title: Text("الملف الشخصي"),
+            subtitle: Text(
+              "عرض وتعديل الملف الشخصي",
+              style: TextStyle(color: Theme.of(context).colorScheme.primary),
+            ),
+          ),
+          SizedBox(height: 10),
+          ListTile(
+            leading: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(Icons.email_outlined),
+              ),
+            ),
+
+            minLeadingWidth: 40,
+            title: Text("البريد الاليكتروني"),
+            subtitle: Text(
+              "ادارة عنوان البريد الاليكتروني",
+              style: TextStyle(color: Theme.of(context).colorScheme.primary),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Text(
+            "التفضيلات",
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          ListTile(
+            dense: true,
+            contentPadding: EdgeInsets.zero,
+            title: Text("الثيم", style: Theme.of(context).textTheme.titleSmall),
+            trailing: DropdownButton(
+              items: [
+                DropdownMenuItem(value: "light", child: Text("الفاتح")),
+                DropdownMenuItem(value: "dark", child: Text("المظلم")),
+                DropdownMenuItem(value: "system", child: Text("النظام")),
+              ],
+              icon: SizedBox(),
+              underline: SizedBox(),
+              isDense: true,
+              value: _themeChoose,
+              onChanged: (value) {
+                setState(() {
+                  _themeChoose = value;
+                });
+              },
+            ),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+            dense: true,
+            title: Text(
+              "الاشعارات",
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            trailing: Transform.scale(
+              scale: .6,
+              child: Switch(
+                value: _notifications,
+                onChanged: (value) {
+                  setState(() {
+                    _notifications = value;
+                  });
+                },
+              ),
+            ),
+          ),
+
+          SizedBox(height: 20),
+          Text(
+            "التفضيلات",
+            style: Theme.of(
+              context,
+            ).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.bold),
+          ),
+          ListTile(
+            contentPadding: EdgeInsets.zero,
+
+            title: Text(
+              "النسخ الاحتياطي والمزامنة",
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            trailing: Icon(Icons.cloud_circle),
+          ),
+        ],
+      ),
+    );
   }
 }
